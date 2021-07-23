adaa61cd71055eb25ed8d47dd3b67f49c45293ec
Bruno Couto <bruno.couto@acct.global>
2021-07-23 17:03:07 -0300

    config webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// * Evitar erros com outros S.O
+const path = require('path');
+// * Obj de configuração
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ]
+    }
+}
